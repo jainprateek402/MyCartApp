a545542bbb7362020d5ece4e32b407e605e132e0
jainprateek402 <40305023+jainprateek402@users.noreply.github.com>
2019-12-20 21:07:41 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='my_project',
+    version='0.1',
+    description='My Project',
+    author='Prateek Jain',
+    author_email='jainprateek402@gmail.com',
+    test_suite='my_project.tests',
+    packages=['my_project'],
+    zip_safe=False,
+)
